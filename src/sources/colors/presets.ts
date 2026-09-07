5ad7bd7d8304f49f486f42b94dfc640eb38234db
risi318 <it.pragmat@gmail.com>
2026-09-07 21:36:17 +0300

    Add ElectroMarks source files

---
+++ src/sources/colors/presets.ts
@@ -0,0 +1,15 @@
+export const backgroundColorPresets = [
+  '#ffffff',
+  '#fff0d5',
+  '#fef3c7',
+  '#ffe0d2',
+  '#def4f2',
+] as const
+
+export const textColorPresets = [
+  '#172033',
+  '#494642',
+  '#1e3a8a',
+  '#854d0e',
+  '#ffffff',
+] as const
